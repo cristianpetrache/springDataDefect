a02a5136927785e12050198f197fa5a07cef1187
Petrut Petrache <petrut.petrache@sap.com>
2019-03-14 21:14:41 +0200

    Set the spring version to 2.0.8.RELEASE and added the Contact PK

--- src/main/java/com/spring/data/bug/demo/model/ContactPK.java
+++ src/main/java/com/spring/data/bug/demo/model/ContactPK.java
@@ -1,4 +1,56 @@
 package com.spring.data.bug.demo.model;
 
-public class ContactPK {
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import java.io.Serializable;
+
+public class ContactPK implements Serializable {
+
+    private static final long serialVersionUID = 2552942475921227466L;
+
+    private String userId;
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        ContactPK contactPK = (ContactPK) o;
+
+        return new EqualsBuilder()
+                .append(userId, contactPK.userId)
+                .append(type, contactPK.type)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(userId)
+                .append(type)
+                .toHashCode();
+    }
 }
