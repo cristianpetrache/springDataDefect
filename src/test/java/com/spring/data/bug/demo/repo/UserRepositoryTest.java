ee7fa89866a153586eb7e73515b43bcf68395ec6
Jens Schauder <jschauder@pivotal.io>
2019-03-15 10:50:00 +0100

    Made test transactional to support lazy loading.

--- src/test/java/com/spring/data/bug/demo/repo/UserRepositoryTest.java
+++ src/test/java/com/spring/data/bug/demo/repo/UserRepositoryTest.java
@@ -10,10 +10,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = DemoApplication.class)
+@Transactional
 public class UserRepositoryTest {
 
 	/**
@@ -25,6 +28,9 @@ public class UserRepositoryTest {
 
 	@Autowired
 	private JpaRepository<User, String> userRepository;
+
+	@Autowired
+	private EntityManager em;
 
 	@Test
 	public void testFindAll_success() {
@@ -43,6 +49,18 @@ public class UserRepositoryTest {
 	public void testFindById_success() {
 
 		User user = userRepository.findById("user1").orElseThrow(RuntimeException::new);
+
+		SoftAssertions.assertSoftly(softly -> {
+					softly.assertThat(user.getContacts()).hasSize(EXPECTED_NO_OF_CONTACTS);
+					softly.assertThat(user.getLanguages()).hasSize(EXPECTED_NO_OF_LANGUAGES);
+				}
+		);
+	}
+
+	@Test
+	public void testEmBaseFindById_success() {
+
+		User user = em.find(User.class,"user1");
 
 		SoftAssertions.assertSoftly(softly -> {
 					softly.assertThat(user.getContacts()).hasSize(EXPECTED_NO_OF_CONTACTS);
